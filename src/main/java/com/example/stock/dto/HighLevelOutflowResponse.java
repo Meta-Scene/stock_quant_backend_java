3a7d2e0e6b0c9ba3307e8ea1aafec6fb65dc4383
myx931 <1773350205@qq.com>
2025-05-19 15:56:15 +0800

    增加了五个每日策略

---
+++ src/main/java/com/example/stock/dto/HighLevelOutflowResponse.java
@@ -0,0 +1,12 @@
+package com.example.stock.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class HighLevelOutflowResponse extends StockResponse {
+  // 所有符合条件的股票代码列表
+  private List<String> ts_codes;
+} 
